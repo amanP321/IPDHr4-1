c1564db75c202fdcf6fa031b30a18d05bc66cbcc
amanP321 <46537428+amanP321@users.noreply.github.com>
2019-01-23 12:23:21 -0600

    Update_for_Team4
    Updated code for team 4

--- team4.py
+++ team4.py
@@ -6,8 +6,8 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+team_name = 'Aman Priyadarshis Team' # Only 10 chars displayed.
+strategy_name = 'Gives a b in the begening and cheaks to see if there are colluding or betraying and gives answers to each'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
@@ -17,7 +17,20 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    if len(my_history) <3:
+        return 'b'
+    if their_history[-1] == 'c' and their_history[-2] == 'c' and their_history[-3] == 'c':
+        return 'b'
+        if their_history[-1] == 'b':
+            return 'c'
+    if their_history[-1] == 'b' and their_history[-2] == 'b' and their_history[-3] == 'b':
+        return 'b'
+    
+            
+            
+    
+        
+     
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -26,7 +39,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -53,7 +66,8 @@ if __name__ == '__main__':
               my_score=0,
               their_score=0,
               result='b'):
-         print('Test passed')
+         print ('Test passed')
+         
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
               their_history='ccc', 
